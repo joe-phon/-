033677c972bfd538c2973ee7680f89ac466f28bb
phon jioe <qiao289824594@foxmail.com>
2020-02-15 22:31:36 +0800

    two

--- robot.py
+++ robot.py
@@ -11,7 +11,7 @@ mynickname = itchat.get_friends()[0]['NickName']
 class Robot:
 #key,请求url
     apiKey = ['98402a996a1f41b7b75aa427ca1b7902','6c293e88435c4ef99b86f8d15ed25a3f','87a7dad6235e0537377e2bb05188f88e']
-    url = 'http://openapi.tuling123.com/openapi/api/v2'
+    url_list = ['http://openapi.tuling123.com/openapi/api/v2',"http://idc.emotibot.com/api/ApiKey/openapi.php"]
     data_json = '''{
         "reqType":0,
         "perception": {
@@ -24,32 +24,44 @@ class Robot:
             "userId": "qiao"
         }
     }'''
+    chat_data = eval('{"cmd": "chat", "appid": apiKey[2], "userid": "qiao", "text": "哈哈", "location": ""}')
 
+    #图灵聊天机器人
     @classmethod
-    def auto_reply(cls,msg):
-        djson=eval(cls.data_json%(msg,random.choice(cls.apiKey)))
-        resp = requests.post(cls.url,json=djson)
+    def tu_reply(cls,msg):
+        djson=eval(cls.data_json%(msg,cls.apiKey[0]))
+        resp = requests.post(cls.url_list[0],json=djson)
         return resp.json()['results'][0]['values']['text']
+
+    #emotibot聊天机器人
+    @classmethod
+    def et_reply(cls,msg):
+        res = requests.post(cls.url_list[1],params=cls.chat_data)
+        return res.json()['data'][0]['value']
+
+    #有消息windou声音提示
     @classmethod
     def alarm(cls):
-    # Windows嗡鸣声
         winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
 
-#监听别人发给我的消息,单个人聊天
+#监听别人发给我的消息,个人聊天
 @itchat.msg_register(itchat.content.TEXT)
 def single_reply(msg):
     print(msg.text)
     if msg['ToUserName'] == myself:
-        res = Robot.auto_reply(msg.text)
+        res = Robot.tu_reply(msg.text)
+        # res = Robot.et_reply(msg.text)
         msg.user.send(res)
         print('I response:',res)
     Robot.alarm()
 
+#监听群聊中@我的消息，群聊
 @itchat.msg_register(TEXT, isGroupChat=True)
 def group_reply(msg):
     if '@%s'%mynickname in msg.text:
         print('%s send me: %s'%(msg.actualNickName, msg.text))
-        res = Robot.auto_reply(msg.text)
+        res = Robot.tu_reply(msg.text)
+        # res = Robot.et_reply(msg.text)
         msg.user.send('@%s %s'%(msg.actualNickName,res))
         print('I response:',res)
     else:
